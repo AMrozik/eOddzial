5847cee726ab38d3c73bf179bdff2f4784ab1939
Kamil Skrzypkowski <glory_shovel@pop-os.localdomain>
2021-11-02 14:11:28 +0100

    Kolejne poprawki do algorytmu: DONE: - poprawka importow - poprawka porownywania operacji (brakuje ciala metody) do sortowania ich - poprawka zbierania danych z DB - sortowanie danych z DB do list wedlug dni

--- api/utils/ALG.py
+++ api/utils/ALG.py
@@ -1,11 +1,15 @@
 # TODO: poprawic nazwe importa
-import Dzien
+import Operation
+import Operation_type
 
-# TODO: wydzielic do oddzielnego pliku
 class Operacja:
     def __init__(self, czas_rozpoczecia, czy_dziecko, czy_trudne):
+        czas_rozpoczecia = self.czas_rozpoczecia
+        czy_dziecko = self.czy_dziecko
+        czy_trudne = self.czy_trudne
 
-    def score:
+    def __cmp__:
+        # TODO: dodac sposob liczenia score
         return 1
 
 
@@ -22,7 +26,7 @@ def podpowiedz(operacje_dnia, lekarz_ID, czy_dziecko, czy_trudne, czas_trwania, 
     czas_przerwy - czas trwania przerwy pooperacyjnej podany w minutch
     """
 
-    #Mozliwe pozycje dla operacji
+    # Mozliwe pozycje dla operacji
     mozliwosci = []
 
     for operacje_w_jednej_sali in enumerate(operacje_dnia):
@@ -30,18 +34,18 @@ def podpowiedz(operacje_dnia, lekarz_ID, czy_dziecko, czy_trudne, czas_trwania, 
 
             # Sprawdzanie przerw miedzy operacjami
             if i+1 < len(operacje_dnia):
-                #sprawdz przerwe miedzy i-ta operacja a i+1
-                przerwa = operacja[rozpoczecie]+czas_trwania - operacje_w_jednej_sali[i][rozpoczecie]
-                #sprawdz czy mozna ustawic operacje w tej przerwie
+                # Sprawdz przerwe miedzy i-ta operacja a i+1
+                przerwa = operacja[rozpoczecie]+czas_trwania - operacje_w_jednej_sali[i+1][rozpoczecie]
+                # Sprawdz czy mozna ustawic operacje w tej przerwie
                 if przerwa + czas_przygotowania > czas_trwania:
-                    #TAK: dodaj operacje do mozliwosci
-                    mozliwosci.append(Operacja(czas_rozpoczecia, czy_dziecko, czy_trudne)) # TODO: sprawdzic konstrukcje
-                #Nie: kontynuuj prace
+                    # TAK: dodaj operacje do mozliwosci
+                    mozliwosci.append(Operacja(czas_rozpoczecia, czy_dziecko, czy_trudne))
+                # Nie: kontynuuj prace
 
-    # TODO: Wyrzuc operacje jezeli lekarz juz zajmuje sie jakas operacja o tej godzinie
+    # TODO: Wyrzuc operacje jezeli lekarz juz zajmuje sie jakas operacja o tej godzinie ale w innej sali
 
-    # Posortuj liste mozliwosci za pomoca score operacji
-    mozliwosci.sort(key=score()) # TODO: sprawdz czy mozna sortowac metoda
+    # Posortuj liste mozliwosci za pomoca wyniku operacji
+    mozliwosci.sort()
 
     return mozliwosci
 
@@ -50,30 +54,34 @@ def podpowiedz(operacje_dnia, lekarz_ID, czy_dziecko, czy_trudne, czas_trwania, 
 if __name__ = '__main__':
     #Dane z zewnatrz
     # TODO: ogarnac zbieranie tych danych
-    czy_dziecko = -1
-    dzien_ID = 123
-    typ_ID = 123
-    # Dane ustalone z gory
-    # TODO: nalezy przechowac te dane i pobierac je
-    czas_przygotowania = 15
-    czas_rozpoczecia_pracy = 480 #czyli 8:00
-    czas_strefy_dzieciecej = 120
-    czas_strefy_trudnej = 120
+    czy_dziecko =
+    dzien_data =
+    typ_ID =
+    # Dane ustalone z gory, przechowywane w ustawieniach Django
+    czas_przygotowania =
+    czas_rozpoczecia_pracy =
+    czas_strefy_dzieciecej =
+    czas_strefy_trudnej =
 
     #---------------------------------------------------------- ZBIERANIE DANYCH
-    # TODO: FIX MERGE ISSUES
-    operacje_dnia = Dzien.object.get(dzien_ID)
-    typ_operacji = Typ_operacji.object.get(typ_ID)
+    # TODO: upewnic sie ze dziala
+    operacje_dnia = Dzien.object.raw("SELECT * FROM myapp_operation WHERE date = $s", [dzien_data])
+    typ_operacji = Operation_type.object.get(typ_ID)
 
     #------------------------------------------------------ PRZYGOTOWANIE DANYCH
     # sortowanie operacji wedlug sal
     lista_salo_operacji = []
     while len(operacje_dnia) > 0:
-        for i in operacje_dnia:
+        temp_list = []
+        sala = operacje_dnia[0][]                               #TODO: uzupelnic
+        for operacja in operacje_dnia:
+            if operacja[] == sala:
+                temp_list.append(operacja)
+                operacje_dnia.remove(operacja) # Mozliwa kolizja petli z usuwaniem
             # 1. wez nr_sali z pierwszej napotkanej sali
             # 2. operacje o tej samej sali wrzuc do listy oraz wyrzuc je z operacji dnia
             # 3. na koniec wrzuc liste do lista_salo_operacji
 
     # TODO: foramatowanie czasu
-
+    # NOTE: wydzielic wszystko na oddzielne funkcje zeby latwo sprawdzac bledy
     sortowane_mozliwosci = podpowiedz()
