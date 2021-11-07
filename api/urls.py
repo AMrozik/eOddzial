ea0982f8a5a44fc0b92630cff6798a4cc75dea8c
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-11-07 14:02:20 +0100

    some more patient views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import PatientsView, CreatePatientsView
+from .views import (
+    PatientsView,
+    CreatePatientsView,
+    patient_by_id,
+    update_patient,
+    delete_patient,
+)
+
 
 urlpatterns = [
-    path('patients', PatientsView.as_view(), name='patients'),
-    path('create_patient', CreatePatientsView.as_view(), name='create_patients'),
+    path('patients/', PatientsView.as_view(), name='patients'),
+    path('create_patient/', CreatePatientsView.as_view(), name='create_patients'),
+    path('patient/<id>/', patient_by_id, name='patient'),
+    path('update_patient/<id>/', update_patient, name='update_patient'),
+    path('delete_patient/<id>/', delete_patient, name='delete_patient'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,16 +1,67 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .serializers import PatientSerializer
-from .models import Patient
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from api.serializers import PatientSerializer
+from api.models import Patient
 
 
-# Create your views here.
+# Patient Views #
+
+# GET only
 class PatientsView(generics.ListAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
 
 
+# POST only
 class CreatePatientsView(generics.CreateAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+
+# GET only
+# function-based view - more customizable i think (or dunno all the options in class-based views)
+@api_view(['GET',])
+def patient_by_id(request, id):
+    try:
+        patient = Patient.objects.get(id=id)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
+
+
+@api_view(['PUT',])
+def update_patient(request, id):
+    try:
+        patient = Patient.objects.get(id=id)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = PatientSerializer(patient, data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data["success"] = "update successful"
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE', ])
+def delete_patient(request, id):
+    try:
+        patient = Patient.objects.get(id=id)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        operation = patient.delete()
+        data = {}
+        if operation:
+            data["success"] = "delete successful"
+        else:
+            data["failure"] = "delete failed"
+        return Response(data=data)
