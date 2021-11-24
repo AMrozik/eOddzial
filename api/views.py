887f6a2e35eb89f8c8af82c8faad6e253be6d618
AMrozik <andrzej.mrozik98@gmail.com>
2021-11-24 17:04:50 +0100

    authorization decorator GIGACHAD

--- api/views.py
+++ api/views.py
@@ -3,11 +3,32 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api.serializers import PatientSerializer
 from api.models import Patient
+from functools import wraps
+from rest_framework_simplejwt.backends import TokenBackend
+from users.models import Account
+
+
+def allow_access(permissions):
+    def allow_access_decorator(func):
+        @wraps(func)
+        def func_wrapper(*args, **kwargs):
+            request = args[0]
+            token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[1]
+            valid_data = TokenBackend(algorithm='HS256').decode(token, verify=False)
+            user_id = valid_data['user_id']
+            user = Account.objects.get(id=user_id)
+            for p in permissions:
+                if user.__dict__[p] is True: break
+            else:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            return func(*args, **kwargs)
+        return func_wrapper
+    return allow_access_decorator
 
 
 # Patient Views #
-
 # GET only
+# @allow_access(permissions=['is_admin']) # nope
 class PatientsView(generics.ListAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
@@ -21,8 +42,10 @@ class CreatePatientsView(generics.CreateAPIView):
 
 # GET only
 # function-based view - more customizable i think (or dunno all the options in class-based views)
-@api_view(['GET',])
+@api_view(['GET', ])
+@allow_access(permissions=['is_admin'])
 def patient_by_id(request, id):
+    print(request)
     try:
         patient = Patient.objects.get(id=id)
     except Patient.DoesNotExist:
@@ -33,7 +56,7 @@ def patient_by_id(request, id):
         return Response(serializer.data)
 
 
-@api_view(['PUT',])
+@api_view(['PUT', ])
 def update_patient(request, id):
     try:
         patient = Patient.objects.get(id=id)
