417ede11fcea88e2dd7a9b58195e535a7c6a83aa
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-12-02 14:14:22 +0100

    Revert "Revert "Authentication n authorization""

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from api.models import Patient, Medic
+from api.models import Patient, Medic, Operation, Operation_type, Room
 
 # Register your models here.
 admin.site.register(Patient)
 admin.site.register(Medic)
+admin.site.register(Room)
+admin.site.register(Operation)
+admin.site.register(Operation_type)
