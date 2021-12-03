b0f85377349fc7b74959a541ac1d96cb71939d22
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-12-03 23:16:30 +0100

    logout finally works as intended

--- front/src/AuthContext.jsx
+++ front/src/AuthContext.jsx
@@ -36,7 +36,14 @@ export const AuthProvider = ({children}) => {
         }
     }
 
-    let logoutUser = () => {
+    let logoutUser = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/logout/', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'refresh':authTokens?.refresh})
+        })
         setAuthTokens(null)
         setUser(null)
         localStorage.removeItem('authTokens')
