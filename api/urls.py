d09667cce5b57b844360be40d65283d4e23d5d41
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-12-16 17:14:42 +0100

    auto-generated api documentation

--- api/urls.py
+++ api/urls.py
@@ -9,6 +9,10 @@ from .views import (
 from rest_framework_simplejwt.views import TokenRefreshView
 from users.views import CustomTokenObtainPairView, BlacklistTokenView
 
+from rest_framework.schemas import get_schema_view
+
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('patients/', PatientsView.as_view(), name='patients'),
@@ -20,5 +24,18 @@ urlpatterns = [
     # JWT
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('logout/', BlacklistTokenView.as_view(), name='logout')
+    path('logout/', BlacklistTokenView.as_view(), name='logout'),
+
+
+    # api documentation for development purposes
+    path('schema/', get_schema_view(
+        title="eOddział",
+        description="API documentation for eOddział",
+        version="1.0.0"
+    ), name='openapi-schema'),
+
+    path('docs/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
 ]
