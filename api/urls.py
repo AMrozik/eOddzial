689421af2daee083e939d8ff64d8c21283db8346
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2021-12-18 16:30:44 +0100

    all GET views for db objects

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,29 @@
 from django.urls import path
 from .views import (
-    PatientsView,
-    CreatePatientsView,
+    all_patients,
+    create_patient,
     patient_by_id,
     update_patient,
     delete_patient,
+
+    medic_by_id,
+    all_medics,
+
+    all_operations,
+    operation_by_id,
+
+    all_rooms,
+    room_by_id,
+
+    all_operation_types,
+    operation_type_by_id,
+
+    all_NAMs,
+    NAM_by_id,
+
+    all_NARs,
+    NAR_by_id,
+
 )
 from rest_framework_simplejwt.views import TokenRefreshView
 from users.views import CustomTokenObtainPairView, BlacklistTokenView
@@ -15,11 +34,29 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('patients/', PatientsView.as_view(), name='patients'),
-    path('create_patient/', CreatePatientsView.as_view(), name='create_patients'),
+    path('patients/', all_patients, name='patients'),
+    path('create_patient/', create_patient, name='create_patients'),
     path('patient/<id>/', patient_by_id, name='patient'),
     path('update_patient/<id>/', update_patient, name='update_patient'),
     path('delete_patient/<id>/', delete_patient, name='delete_patient'),
+
+    path('medic/<id>/', medic_by_id, name='medic'),
+    path('medics/', all_medics, name='medics'),
+
+    path('operations/', all_operations, name='medics'),
+    path('operation/<id>/', operation_by_id, name='medic'),
+
+    path('rooms/', all_rooms, name='rooms'),
+    path('rooms/<id>/', room_by_id, name='room'),
+
+    path('operation_types/', all_operation_types, name='operation_types'),
+    path('operation_type/<id>/', operation_type_by_id, name='operation_type'),
+
+    path('not_available_rooms/', all_NARs, name='operation_types'),
+    path('not_available_room/<id>/', NAR_by_id, name='operation_type'),
+
+    path('not_available_medics/', all_NAMs, name='operation_types'),
+    path('not_available_medic/<id>/', NAM_by_id, name='operation_type'),
 
     # JWT
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -32,10 +69,10 @@ urlpatterns = [
         title="eOddział",
         description="API documentation for eOddział",
         version="1.0.0"
-    ), name='openapi-schema'),
+    ),  name='openapi-schema'),
 
     path('docs/', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url':'openapi-schema'}
-    ), name='swagger-ui'),
+    ),  name='swagger-ui'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,32 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from api.serializers import PatientSerializer
-from api.models import Patient
 from functools import wraps
 from rest_framework_simplejwt.backends import TokenBackend
 from users.models import Account
 from django.core.exceptions import ValidationError
 from typing import List
+
+
+from api.serializers import (
+    PatientSerializer,
+    MedicSerializer,
+    OperationSerializer,
+    OperationTypeSerializer,
+    NonAvailabilityMedicSerializer,
+    RoomSerializer,
+    NonAvailabilityRoomSerializer,
+)
+
+from .models import (
+    Patient,
+    Medic,
+    NonAvailabilityMedic,
+    Operation,
+    Operation_type,
+    Room,
+    NonAvailabilityRoom,
+)
 
 
 def retrieve_user(request):
@@ -66,25 +85,35 @@ def allow_access(permissions: List[str]):
 
 
 # Patient Views #
-# GET only
-# @allow_access(permissions=['is_admin']) # nope
-class PatientsView(generics.ListAPIView):
-    queryset = Patient.objects.all()
-    serializer_class = PatientSerializer
+@api_view(['GET', ])
+@allow_access(permissions=['is_admin'])
+def all_patients(request, id):
+    try:
+        patient = Patient.objects.get(id=id)
+    except Patient.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PatientSerializer(patient)
+        return Response(serializer.data)
 
 
-# POST only
-class CreatePatientsView(generics.CreateAPIView):
-    queryset = Patient.objects.all()
-    serializer_class = PatientSerializer
+@api_view(['POST', ])
+@allow_access(permissions=['is_admin'])
+def create_patient(request):
+    if request.method == 'POST':
+        serializer = PatientSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+            serializer.save()
+            data["success"] = "update successful"
+            return Response(data=data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# GET only
-# function-based view - more customizable i think (or dunno all the options in class-based views)
 @api_view(['GET', ])
 @allow_access(permissions=['is_admin'])
 def patient_by_id(request, id):
-    print(request)
     try:
         patient = Patient.objects.get(id=id)
     except Patient.DoesNotExist:
@@ -127,3 +156,135 @@ def delete_patient(request, id):
         else:
             data["failure"] = "delete failed"
         return Response(data=data)
+
+
+# Medic Views #
+@api_view(['GET', ])
+def medic_by_id(request, id):
+    try:
+        medic = Medic.objects.get(id=id)
+    except Medic.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MedicSerializer(medic)
+        return Response(serializer.data)
+
+
+@api_view(['GET', ])
+def all_medics(request):
+    medics = [medic for medic in Medic.objects.all()]
+
+    if request.method == 'GET':
+        serializer = MedicSerializer(medics, many=True)
+        return Response(serializer.data)
+
+
+# Operation Views #
+@api_view(['GET', ])
+def all_operations(request):
+    operations = [operation for operation in Operation.objects.all()]
+
+    if request.method == 'GET':
+        serializer = OperationSerializer(operations, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', ])
+def operation_by_id(request, id):
+    try:
+        operation = Operation.objects.get(id=id)
+    except Medic.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = OperationSerializer(operation)
+        return Response(serializer.data)
+
+
+# Room Views #
+@api_view(['GET', ])
+def all_rooms(request):
+    rooms = [room for room in Room.objects.all()]
+
+    if request.method == 'GET':
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', ])
+def room_by_id(request, id):
+    try:
+        room = Room.objects.get(id=id)
+    except Room.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = RoomSerializer(room)
+        return Response(serializer.data)
+
+
+# Operation_type Views #
+@api_view(['GET', ])
+def all_operation_types(request):
+    operation_types = [operation_type for operation_type in Operation_type.objects.all()]
+
+    if request.method == 'GET':
+        serializer = OperationTypeSerializer(operation_types, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', ])
+def operation_type_by_id(request, id):
+    try:
+        operation_type = Operation_type.objects.get(id=id)
+    except Operation_type.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = OperationTypeSerializer(operation_type)
+        return Response(serializer.data)
+
+
+# NonAvailabilityMedic Views #
+@api_view(['GET', ])
+def all_NAMs(request):
+    NAMs = [NAM for NAM in NonAvailabilityMedic.objects.all()]
+
+    if request.method == 'GET':
+        serializer = NonAvailabilityMedicSerializer(NAMs, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', ])
+def NAM_by_id(request, id):
+    try:
+        NAM = NonAvailabilityMedic.objects.get(id=id)
+    except NAM.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = NonAvailabilityMedicSerializer(NAM)
+        return Response(serializer.data)
+
+
+# NonAvailabilityRoom Views #
+@api_view(['GET', ])
+def all_NARs(request):
+    NARs = [NAR for NAR in NonAvailabilityRoom.objects.all()]
+
+    if request.method == 'GET':
+        serializer = NonAvailabilityRoomSerializer(NARs, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', ])
+def NAR_by_id(request, id):
+    try:
+        NAR = NonAvailabilityRoom.objects.get(id=id)
+    except NAR.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = NonAvailabilityRoomSerializer(NAR)
+        return Response(serializer.data)
