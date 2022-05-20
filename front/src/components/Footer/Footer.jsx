f6d9a001ed2c60b95a8a75d863ccdfb2e50f9419
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2022-05-20 17:06:26 +0200

    WSZYSTKOOOO CZEGO TYLKO DOTKNEEE, TO STAJE SIE ZŁOTEEEM

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 export const Footer = () => {
   return (
       <div className="footer">
-        &copy; {new Date().getFullYear()} <a href="https://www.nfz.gov.pl/dla-swiadczeniodawcy/slowniki/pliki-icd-9-pl/" className="repo_link"> Government announcement regarding available operations in hospitals </a>
+        &copy; {new Date().getFullYear()} <a href="https://www.nfz.gov.pl/dla-swiadczeniodawcy/slowniki/pliki-icd-9-pl/" className="repo_link"> Ogłoszenie rządu dotyczące dostępnych operacji w szpitalach </a>
       </div>
   )
 };
